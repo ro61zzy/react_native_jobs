b1550a4031f70e4fa5fd11fcf2b90228845a887a
rozzy <rosekaremeri@gmail.com>
2023-08-15 11:38:58 +0300

    Feature: Welcome page

--- app/_layout.js
+++ app/_layout.js
@@ -9,9 +9,19 @@ SplashScreen.preventAutoHideAsync()
 const Layout = () => {
 const [fontsLoaded] = useFonts({
     DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
 })
 
-  return <Stack />
+const onLayoutRootView = useCallback(async () => {
+    if(fontsLoaded){
+        await SplashScreen.hideAsync()
+    }
+}, [fontsLoaded])
+if(!fontsLoaded) return null;
+
+
+  return <Stack onLayout={onLayoutRootView} />
 }
 
 export default Layout
