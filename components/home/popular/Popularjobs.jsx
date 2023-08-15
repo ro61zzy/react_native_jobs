a58544db9fdb88f25f9ceaa6356ad8907332f7de
rozzy <rosekaremeri@gmail.com>
2023-08-15 14:52:44 +0300

    Feature: setting up job boards

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
 
+import { useRouter } from 'expo-router'
+import { icons, images, SIZES, COLORS } from '../../../constants'
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 import styles from './popularjobs.style'
 
 const Popularjobs = () => {
+  const router = useRouter()
+  const isLoading = false
+  const error = false
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity><Text style={styles.headerBtn}>Show All</Text></TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something Went Wrong</Text>
+        ) : (
+          <FlatList
+            data={[1, 2, 3, 4, 5, 6, 7, 8 ]}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+              />
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )
+        }
+      </View>
     </View>
   )
 }
---
+++ hook/useFetch.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { View } from "react-native-web";
+import { RAPID_API_KEY } from "@env";
+
+const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [erroe, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": rapidApiKey,
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: {
+      ...query,
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      alert("There is a error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+useEffect(()=>{
+    fetchData();
+}, [])
+
+};
