5f201b8405b7c788f52b4702850c598a172b408a
rozzy <rosekaremeri@gmail.com>
2023-08-15 10:31:21 +0300

    Features: Add components

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {useCallback} from 'react'
 import { View, Text } from 'react-native'
 import { Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync()
 
 const Layout = () => {
+const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+})
+
   return <Stack />
 }
 
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from 'react-native'
+import { useRouter } from 'expo-router'
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+
+  const router = useRouter()
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Rozz</Text>
+        <Text style={styles.welcomeMessage}>Find Your Perfect Job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
     </View>
   )
 }
